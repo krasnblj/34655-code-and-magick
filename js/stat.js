61c3779cea6fbe48be5ed5454500f5cc14cba9e9
krasnblj <krasnblj@mail.ru>
2018-09-04 00:09:34 +0300

    Добавил скрипт и вывел облако

---
+++ js/stat.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var window.renderStatistics = function(ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = 'rgb(255,255,255)';
+  ctx.fillRect(100, 10, 420, 270);
+};
